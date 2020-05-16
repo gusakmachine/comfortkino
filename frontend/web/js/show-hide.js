db6bc92f0246accc11f05466b283935f8c488a11
Dmitriy Propastin <dima.propastin@gmail.com>
2020-05-16 20:18:18 +0300

    adaptability film page

--- frontend/web/js/show-hide.js
+++ frontend/web/js/show-hide.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
     // Show-hide
     $('body').on('click', '[data-SH]', function () {
-        if ($($(this).data('sh')).css('display') == 'none')
+        if ($($(this).data('sh')).css('display') === 'none') {
             $($(this).data('sh')).fadeIn().css('display', 'flex');
-        else $($(this).data('sh')).fadeOut().css('display', 'flex');
+            $('body').css('overflow','hidden');
+        } else {
+            $($(this).data('sh')).fadeOut().css('display', 'flex')
+            $('body').css('overflow','unset');
+        }
     });
 });
